6639504f6afb7811e5e38bc51a1ae5a40eedfdfa
devcronberg <devcronberg@gmail.com>
2019-05-03 12:37:14 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,2 @@
-copy *.snippet "C:\Users\admin\Documents\Visual Studio 2017\Code Snippets\Visual C#\My Code Snippets"
+rem copy *.snippet "C:\Users\admin\Documents\Visual Studio 2017\Code Snippets\Visual C#\My Code Snippets"
+copy *.snippet "C:\Users\admin\Documents\Visual Studio 2019\Code Snippets\Visual C#\My Code Snippets"
